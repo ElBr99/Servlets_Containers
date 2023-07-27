f657a5e385a74345aa902f2926621dbfd2ba2d0a
ElBr99 <iershov@okta-solutions.com>
2023-07-27 16:30:12 +0300

    "Di-annotation"

--- servlets/src/main/java/ru/netology/controller/PostController.java
+++ servlets/src/main/java/ru/netology/controller/PostController.java
@@ -1,11 +1,14 @@
 package ru.netology.controller;
 
+import org.springframework.stereotype.Controller;
 import ru.netology.model.Post;
 import ru.netology.repository.PostRepository;
 import ru.netology.service.PostService;
 
+@Controller
 public class PostController extends GenericController<Post> {
-    public PostController() {
-        super(new PostService(new PostRepository()));
+
+    public PostController(PostService service) {
+        super(service);
     }
 }
